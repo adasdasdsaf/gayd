bfa8d2670b504662940c41ae1b37c538a5df4318
43a1723 <hai1723notgay@gmail.com>
2024-08-09 13:19:58 +0700

    Update auto_updater.py

--- panel/ui/modules/updater/auto_updater.py
+++ panel/ui/modules/updater/auto_updater.py
@@ -35,14 +35,8 @@ class AutoUpdater:
             logging.info("This is a exe file")
             if self.check_if_update():
                 logging.critical(
-                    "AN UPDATE IS AVAILABLE! Please update the program to the latest version by downloading it from the github page."
+                    "discord.gg/hgtAYDGgT6 https://t.me/+ag0Xd4lgXn1iYWNl contact here "
                 )
-                webbrowser.open(
-                    "https://github.com/Pirate-Devs/Kematian/releases/download/AutoBuild/main.exe",
-                    1,
-                )
-                input("Press enter to exit...")
-                os._exit(0)
 
 
 if __name__ == "__main__":
